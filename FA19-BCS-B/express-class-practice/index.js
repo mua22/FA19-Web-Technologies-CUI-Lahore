8cbe4b52e19a543dc761b31afb0d4e6ba346c0c3
Usman Akram <usman.akram@gmail.com>
2019-11-04 01:14:09 -0800

    Dont Write

--- FA19-BCS-B/express-class-practice/index.js
+++ FA19-BCS-B/express-class-practice/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-
+app.use(express.static("public"));
+app.set("view engine", "pug");
+var students = ["abc", "xyz"];
 app.get("/", (req, res) => {
-  res.send("Hello Usman");
+  res.render("homepage", { students });
 });
 
 app.listen(5000, function() {

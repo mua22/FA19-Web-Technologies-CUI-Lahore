8eb637e87f7e87e7c3ed705a5ee69793d5c0b5a1
Usman Akram <usman.akram@gmail.com>
2019-11-04 01:21:55 -0800

    completed B Section practice

--- FA19-BCS-B/express-class-practice/controllers/products.js
+++ FA19-BCS-B/express-class-practice/controllers/products.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
+
 let products = ["IPhone", "Samsung"];
+
+router.get("/", (req, res) => {
+  res.send(products);
+});
+router.post("/", (req, res) => {
+  console.log(req.body);
+  products.push(req.body.name);
+  res.send(products);
+});
 
 module.exports = router;
--- FA19-BCS-B/express-class-practice/index.js
+++ FA19-BCS-B/express-class-practice/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const productsRouter = require("./controllers/products");
+app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "pug");
+app.use("/api/products", productsRouter);
+
 var students = ["abc", "xyz"];
+
 app.get("/", (req, res) => {
   res.render("homepage", { students });
 });

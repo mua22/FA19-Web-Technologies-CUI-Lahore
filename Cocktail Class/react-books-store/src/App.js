1f9538bbbb0b20724787471b9b2cd0cad1c77f1e
Usman Akram <usman.akram@gmail.com>
2019-11-09 01:00:32 -0800

    cocktail class react

---
+++ Cocktail Class/react-books-store/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from App</h1>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
